4c52f0b7e45c32ab7c68534d0a78bf7f8562f454
Fredy Nur Apriyanto <mail.fredyna@gmail.com>
2018-02-21 18:17:58 +0700

    tambah file profil

---
+++ profil.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = "Fredy Nur Apriyanto";
+    $kelas = "4C";
+    $prodi = "D4 Teknik Informatika";
+
+    echo "<h1>Latihan Mata Kuliah Web Programming<h1><br>";
+    echo $nama; echo "<br>";
+    echo $kelas; echo "<br>";
+    echo $prodi; echo "<br>";
+?>
