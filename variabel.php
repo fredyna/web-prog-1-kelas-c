cacafa38e27497bb3bfb39ac6b9ec2fba65eec0c
Fredy Nur Apriyanto <mail.fredyna@gmail.com>
2018-02-21 17:20:11 +0700

    tambah file variabel.php

---
+++ variabel.php
@@ -0,0 +1,16 @@
+<?php
+    $nama = "Fredy Nur Apriyanto";
+    $usia = 20;
+    $honor = 10.5;
+    $statusMenikah = false;
+    $kosong = null;
+
+    var_dump($nama); echo "<br>";
+    var_dump($usia); echo "<br>";
+    var_dump($honor); echo "<br>";
+    var_dump($statusMenikah); echo "<br>";
+    var_dump($kosong); echo "<br></br>";
+
+    $statusMenikah = "Menikah";
+    var_dump($statusMenikah);
+?>
