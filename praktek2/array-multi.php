e61b84ad9d69fd9c7b087267984f3750d1cf4212
Fredy Nur Apriyanto <mail.fredyna@gmail.com>
2018-02-28 16:45:07 +0700

    tambah file array-multi.php

---
+++ praktek2/array-multi.php
@@ -0,0 +1,17 @@
+<?php
+    $data = array(
+        "web-prog" => array(
+            "fredy" => 9,
+            "didi"  => 7,
+            "eko"   => 8
+        ),
+        "kalkulus" => array(
+            "fredy" => 8,
+            "didi"  => 9,
+            "eko"   => 7
+        )
+    );
+
+    echo "Nilai web-prog untuk Fredy : " . $data["web-prog"]["fredy"] . "<br>";
+    echo "Nilai kalkulus untuk Eko : " . $data["kalkulus"]["eko"] . "<br>";
+?>
